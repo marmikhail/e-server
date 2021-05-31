cc5cf72488cf09b5bb331db4b6ff0169ab993f40
m1909452 <81954904+m1909452@users.noreply.github.com>
2021-05-31 12:03:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const db             = require('./config/db');
 const app            = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
