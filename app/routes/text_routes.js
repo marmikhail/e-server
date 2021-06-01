b5f186b15203973d906f958db61e77a529c5f772
m1909452 <81954904+m1909452@users.noreply.github.com>
2021-06-01 14:56:07 +0300

    Update text_routes.js

--- app/routes/text_routes.js
+++ app/routes/text_routes.js
@@ -1,21 +1,9 @@
 module.exports = function(app, db) {
-  app.get('/name/:name', (req, res) => {
-    const name = req.params.name;
-    const query = { '$text' : {'$search' : name}};
+  app.get('/:find', (req, res) => {
+    const find = req.params.find;
+    const query = { $text : {$search : find}};
     const options = { 'information' : 1, '_id': 0 };
-    db.collection('database').createIndex({name : "text"});
-    db.collection('database').findOne(query, options, (err, item) => {
-      if (err) {
-        res.send({'error':'An error has occurred'});
-      } else {
-        res.send(item);
-      } 
-    });
-  });
-  app.get('/number/:number', (req, res) => {
-    const number = req.params.number;
-    const query = { number : number}
-    const options = { information : 1, _id: 0};
+    db.collection('database').createIndex({number: "text", name: "text"});
     db.collection('database').findOne(query, options, (err, item) => {
       if (err) {
         res.send({'error':'An error has occurred'});
